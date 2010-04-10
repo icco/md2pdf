82fc20a1ec007b55e73731dcd708f48e92d5b18e
Nat Welch <nat@natwelch.com>
2010-04-10 13:15:06 -0700

    Much better, still looks a little funky tho.

--- md2pdf.rb
+++ md2pdf.rb
@@ -6,8 +6,13 @@ require 'htmldoc'
 pdf = PDF::HTMLDoc.new
 
 pdf.set_option :outfile, "./outfile.pdf"
-pdf.set_option :links, true
+pdf.set_option :bodycolor, :white
 pdf.set_option :toc, false
+pdf.set_option :portrait, true
+pdf.set_option :links, false
+pdf.set_option :webpage, true
+pdf.set_option :left, '2cm'
+pdf.set_option :right, '2cm'
 
 pdf.header ".t."
 md = RDiscount.new(File.read(ARGV[0]), :smart)
