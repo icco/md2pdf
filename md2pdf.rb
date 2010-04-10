076310edb9673b569205067b8c0a9eca59b37f48
Nat Welch <nat@natwelch.com>
2010-04-10 13:33:11 -0700

    Adds command line checking.

--- md2pdf.rb
+++ md2pdf.rb
@@ -3,9 +3,23 @@
 require 'rdiscount'
 require 'htmldoc'
 
-pdf = PDF::HTMLDoc.new
+if ARGV.length != 2
+   puts "md2pdf : Markdown to PDF converter."
+   puts "usage  : md2pdf infile outfile"
 
-pdf.set_option :outfile, "./outfile.pdf"
+   exit 1
+end
+
+infile = ARGV[0]
+outfile = ARGV[1]
+
+if !File.exists? infile
+   puts "#{infile} does not exist."
+   exit 2
+end
+
+pdf = PDF::HTMLDoc.new
+pdf.set_option :outfile, outfile
 pdf.set_option :bodycolor, :white
 pdf.set_option :toc, false
 pdf.set_option :portrait, true
@@ -15,7 +29,7 @@ pdf.set_option :left, '2cm'
 pdf.set_option :right, '2cm'
 
 pdf.header ".t."
-md = RDiscount.new(File.read(ARGV[0]), :smart)
+md = RDiscount.new(File.read(infile), :smart)
 pdf << md.to_html
 
 pdf.footer ".1."
